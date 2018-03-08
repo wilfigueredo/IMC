2551e1439a12294cb5332f514b25af8c49f96059
wilfigueredo <william_figueredo@hotmail.com>
2018-03-08 11:02:22 -0300

    Calculo IMC

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Validator, FormBuilder, FormGroup, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public form : FormGroup;
+  constructor(private fb : FormBuilder) {
+    this.form = this.fb.group({
 
+      peso: ['', Validators.compose([
+        Validators.minLength(1),
+        Validators.maxLength(10),
+        Validators.required
+      ])],
+      altura: ['',Validators.compose([
+        Validators.minLength(1),
+        Validators.maxLength(10),
+        Validators.required
+      ])]
+    });
+  }
+
+  calcularIMC(){
+    let peso = this.form.controls['peso'].value;
+    let altura = this.form.controls['altura'].value;
+    altura = altura / 100;
+    altura = altura * altura;
+    let imc = peso/altura;
+    let resultado = "";
+
+    if(imc < 17)
+      resultado = "Muito abaixo do peso";
+    else if(imc > 17 && imc < 18.5)
+      resultado = "Abaixo do peso";
+    else if(imc > 18.4 && imc < 25)
+      resultado = "Peso ideal";
+    else if(imc > 25 && imc < 30)
+      resultado = "Sobrepeso";  
+    else if(imc >= 30 && imc < 35)
+      resultado = "Obesidade";   
+    else if(imc >= 35 && imc <= 40)
+      resultado = "Obesidade grave";
+    else if(imc > 40)
+      resultado = "Obesidade Morbida";     
+    alert(imc.toFixed(2) + "\n" + resultado);
   }
 
 }
